88fd634c56f367a084e2a4303320aef5c9f970e4
Gilvam Mourão <gilvamm@gmail.com>
2018-10-31 20:01:30 -0300

    Criando Componente Usando ControlValueAccessor (Parte 2) Seção 6, aula 64

--- meat-app-starter/src/app/_shared/component/radio/radio.component.ts
+++ meat-app-starter/src/app/_shared/component/radio/radio.component.ts
@@ -1,23 +1,67 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
 import { RadioOption } from '../../model/radio-option.model';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'mt-radio',
-  templateUrl: './radio.component.html'
+  templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
 
   @Input() options: RadioOption[];
 
   value: any;
+  onChange: any;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
   setValue(value: any) {
     this.value = value;
+    this.onChange(this.value);
+  }
+
+  /**
+   * -- ControlValueAccessor -------
+   * Write a new value to the element.
+   */
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  /**
+   * -- ControlValueAccessor -------
+   * Set the function to be called when the control receives a change event.
+   */
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  /**
+   * -- ControlValueAccessor -------
+   * Set the function to be called when the control receives a touch event.
+   */
+  registerOnTouched(fn: any): void {
+  }
+
+  /**
+   * -- ControlValueAccessor -------
+   * This function is called when the control status changes to or from "DISABLED".
+   * Depending on the value, it will enable or disable the appropriate DOM element.
+   *
+   * @param isDisabled
+   */
+  setDisabledState?(isDisabled: boolean): void {
   }
 
 }
